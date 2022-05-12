fe4bc122fcec200b08c1c22c0939d60eaccfd468
Mateusz Barwicki <mateuszbarwicki2@gmail.com>
2022-05-12 16:31:29 +0200

    Add imports and app structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  const orderMealHandler = () => {
+    console.log("ordering...");
+    setCartIsShown(false);
+  };
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <CartProvider>
+      {cartIsShown && (
+        <Cart onCloseCart={hideCartHandler} onOrder={orderMealHandler} />
+      )}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
